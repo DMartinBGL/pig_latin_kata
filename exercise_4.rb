b5abf0af745762120d3668ba43250cd2c1839731
Joe Ball <joe.ball@bglgroup.co.uk>
2019-10-08 16:57:22 +0100

    exercise 4

--- exercise_4.rb
+++ exercise_4.rb
@@ -6,7 +6,23 @@
 # other formats
 
 def pig_latin(word)
-
+  word_array = word.split("")
+  if word_array[0] =~ /[A-Z]/
+    capitalise = true
+  end
+  if word.start_with?("a","e","i","o","u")
+    return "#{word}way"
+  else
+    if word_array[0] == "q" and word_array[1] == "u"
+      word_array = word_array.rotate(2)
+    else
+      until word_array[0].start_with?("a","e","i","o","u")
+        word_array = word_array.rotate(1)
+      end
+    end
+    return "#{word_array.join}ay" unless capitalise == true
+      "#{word_array.join.downcase.capitalize!}ay"
+  end
 end
 
 ## Tests:
